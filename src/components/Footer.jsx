a2bad1393736754b8938195babf6294ba6027fb2
tienpbss <tienphung.cn07@gmail.com>
2023-11-07 09:23:08 +0700

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,17 @@
 function Footer() {
   return (
-    <footer>
-      <p>
-        &copy; 2023 Your Blog. All rights reserved. |{" "}
-        <a href="privacy-policy.html">Privacy Policy</a> |{" "}
-        <a href="contact.html">Contact Us</a>
-      </p>
+    <footer
+      className="border p-3 mt-auto"
+      style={{ backgroundColor: "#f8f9fa" }}
+    >
+      <div className="container text-center">
+        <p className="p-0 m-0">
+          {" "}
+          An copy of <a href="http://demo.realworld.io/#/">realworld</a>{" "}
+          project (see <a href="https://github.com/gothinkster/realworld">more</a>). Made by{" "}
+          <a href="https://github.com/tienpbss">Tien</a> .
+        </p>
+      </div>
     </footer>
   );
 }
