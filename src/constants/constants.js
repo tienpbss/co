c5569afc96d8703ac09e9cb5944ae8b80e2ad846
tienpbss <tienphung.cn07@gmail.com>
2023-11-10 17:52:26 +0700

    fix bug missing api in base url

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL
+export const BASE_URL = `${import.meta.env.VITE_BASE_URL}/api`
 // export const BASE_URL='https://api.realworld.io/api'
 export const defaultImg = 'https://api.realworld.io/images/smiley-cyrus.jpeg'
